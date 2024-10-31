1c1c399aded55315ef0c7ecc423ce535100ad9d2
Max Zarev <maksimelizarev@gmail.com>
2024-10-31 12:02:32 +0700

    задачи по типу данных

---
+++ Основы  Python/Типы данных/solution/task_2.py
@@ -0,0 +1,8 @@
+str
+
+
+def __add__(self, *args, **kwargs): # real signature unknown
+    """ Return self+value. """
+    # она возвращает себя и справа подставляет второе значение, соеденяет строки
+    pass
+
---
+++ Основы  Python/Типы данных/solution/task_6.py
@@ -0,0 +1,8 @@
+text = """Ja pierdolę, patrzcie co spotkałem. Bóbr, kurwa! Ja pierdolę, 
+jakie bydlę! Bóbr! Ej, kurwa, bóbr! Bóbr, nie spierdalaj, mordo!
+Chodź tu, kurwa, do mnie, bóbr! Ale jesteś, kurwa, duży ty! Bóbr!
+Ja pierdolę, pierwszy raz w życiu widzę bobra! Jakie bydlę jebane,
+spierdolił do wody i się utopił!"""
+
+print(text.__len__())  # выводит количество символов в тексте, вызывая функцию len()
+print(len(text))  # обычно используемый способ выводить количество символов в тексте
