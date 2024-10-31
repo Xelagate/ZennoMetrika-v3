1f08464bdc9c8b92e2a50905759b114cbaa6802f
Max Zarev <maksimelizarev@gmail.com>
2024-10-31 14:23:16 +0700

    задачи по int

---
+++ Основы  Python/Тип данных int/main.py
@@ -0,0 +1,6 @@
+# 1
+number_one = '10'
+number_two = '20'
+number_one = int(number_one)  # преобразовал строку в число
+number_two = int(number_two)  # преобразовал строку в число
+print(number_one + number_two)
