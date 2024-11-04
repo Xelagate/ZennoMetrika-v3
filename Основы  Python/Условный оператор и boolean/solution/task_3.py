e25115edc0a8c0eab1540a9d439de49bbdccf50f
Max Zarev <maksimelizarev@gmail.com>
2024-11-04 21:34:15 +0700

    задачи по if elif else

--- Основы  Python/Условный оператор и boolean/solution/task_3.py
+++ Основы  Python/Условный оператор и boolean/solution/task_3.py
@@ -1,112 +1,162 @@
-"""
-У вас есть 2 переменные с балансами кошелька в токене ETH и USDT.
-Еще одна переменная описывает стоимость ETH в USDT.
-
-Напишите программу, которая будет делать обмен из ETH в USDT если баланс USDT нулевой, при этом оставляя 5% токенов ETH на комиссии.
-
-Если баланс USDT не нулевой, то программа должна делать обмен всех токенов USDT в ETH.
-
-Во время обмена должно печататься в терминале какой токен меняется на какой и какая сумма обмена.
-По результату обмена программа балансы должны меняться.
-
-Продублируйте логику, чтобы программа делала 5 обменов из ETH в USDT и обратно, сама выбирая когда и какой обмен делать.
-
-В конце работы программа должна выводить сообщение актуальный баланс токенов ETH и USDC.
-
-"""
-
+# Начальный баланс в ETH и USDT
 balance_eth = 1.35165161
 balance_usdt = 505
+# Текущая цена ETH в USDT
 price_eth = 3000
 
+# Проверяем, если баланс USDT равен 0
 if balance_usdt == 0:
-    commission = balance_eth * 0.05  # 5% на будущие комиссии
-    eth_amount_in = balance_eth - commission  # сумма обмена ETH
-    usdt_amount_out = eth_amount_in * price_eth # сумма обмена USDT
+    # Рассчитываем комиссию в 5% от текущего баланса ETH
+    commission = balance_eth * 0.05  
+    # Рассчитываем количество ETH для обмена, вычитая комиссию
+    eth_amount_in = balance_eth - commission  
+    # Рассчитываем количество USDT, получаемое от обмена ETH
+    usdt_amount_out = eth_amount_in * price_eth 
+    # Выводим информацию об обмене
     print(f'Обмен {balance_eth} ETH на {usdt_amount_out} USDT')
+    # Обновляем баланс USDT после обмена
     balance_usdt += usdt_amount_out
+    # Обновляем баланс ETH после обмена
     balance_eth -= eth_amount_in
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 else:
+    # Рассчитываем количество ETH, которое можно купить на текущий баланс USDT
     eth_amount_in = balance_usdt / price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_usdt} USDT на {eth_amount_in} ETH')
+    # Обновляем баланс ETH после обмена
     balance_eth += eth_amount_in
+    # Устанавливаем баланс USDT в 0 после обмена
     balance_usdt = 0
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 
-print()
+print()  # Пустая строка для разделения выводов
 
+# Повторяем тот же процесс обмена для демонстрации
 if balance_usdt == 0:
-    commission = balance_eth * 0.05  # 5% на будущие комиссии
-    eth_amount_in = balance_eth - commission  # сумма обмена ETH
-    usdt_amount_out = eth_amount_in * price_eth # сумма обмена USDT
+    # Рассчитываем комиссию в 5% от текущего баланса ETH
+    commission = balance_eth * 0.05
+    # Рассчитываем количество ETH для обмена, вычитая комиссию
+    eth_amount_in = balance_eth - commission
+    # Рассчитываем количество USDT, получаемое от обмена ETH
+    usdt_amount_out = eth_amount_in * price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_eth} ETH на {usdt_amount_out} USDT')
+    # Обновляем баланс USDT после обмена
     balance_usdt += usdt_amount_out
+    # Обновляем баланс ETH после обмена
     balance_eth -= eth_amount_in
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 else:
+    # Рассчитываем количество ETH, которое можно купить на текущий баланс USDT
     eth_amount_in = balance_usdt / price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_usdt} USDT на {eth_amount_in} ETH')
+    # Обновляем баланс ETH после обмена
     balance_eth += eth_amount_in
+    # Устанавливаем баланс USDT в 0 после обмена
     balance_usdt = 0
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 
-print()
+print()  # Пустая строка для разделения выводов
+
 if balance_usdt == 0:
-    commission = balance_eth * 0.05  # 5% на будущие комиссии
-    eth_amount_in = balance_eth - commission  # сумма обмена ETH
-    usdt_amount_out = eth_amount_in * price_eth # сумма обмена USDT
+    # Рассчитываем комиссию в 5% от текущего баланса ETH
+    commission = balance_eth * 0.05
+    # Рассчитываем количество ETH для обмена, вычитая комиссию
+    eth_amount_in = balance_eth - commission
+    # Рассчитываем количество USDT, получаемое от обмена ETH
+    usdt_amount_out = eth_amount_in * price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_eth} ETH на {usdt_amount_out} USDT')
+    # Обновляем баланс USDT после обмена
     balance_usdt += usdt_amount_out
+    # Обновляем баланс ETH после обмена
     balance_eth -= eth_amount_in
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 else:
+    # Рассчитываем количество ETH, которое можно купить на текущий баланс USDT
     eth_amount_in = balance_usdt / price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_usdt} USDT на {eth_amount_in} ETH')
+    # Обновляем баланс ETH после обмена
     balance_eth += eth_amount_in
+    # Устанавливаем баланс USDT в 0 после обмена
     balance_usdt = 0
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 
-print()
+print()  # Пустая строка для разделения выводов
+
 if balance_usdt == 0:
-    commission = balance_eth * 0.05  # 5% на будущие комиссии
-    eth_amount_in = balance_eth - commission  # сумма обмена ETH
-    usdt_amount_out = eth_amount_in * price_eth # сумма обмена USDT
+    # Рассчитываем комиссию в 5% от текущего баланса ETH
+    commission = balance_eth * 0.05
+    # Рассчитываем количество ETH для обмена, вычитая комиссию
+    eth_amount_in = balance_eth - commission
+    # Рассчитываем количество USDT, получаемое от обмена ETH
+    usdt_amount_out = eth_amount_in * price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_eth} ETH на {usdt_amount_out} USDT')
+    # Обновляем баланс USDT после обмена
     balance_usdt += usdt_amount_out
+    # Обновляем баланс ETH после обмена
     balance_eth -= eth_amount_in
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 else:
+    # Рассчитываем количество ETH, которое можно купить на текущий баланс USDT
     eth_amount_in = balance_usdt / price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_usdt} USDT на {eth_amount_in} ETH')
+    # Обновляем баланс ETH после обмена
     balance_eth += eth_amount_in
+    # Устанавливаем баланс USDT в 0 после обмена
     balance_usdt = 0
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 
-print()
+print()  # Пустая строка для разделения выводов
+
 if balance_usdt == 0:
-    commission = balance_eth * 0.05  # 5% на будущие комиссии
-    eth_amount_in = balance_eth - commission  # сумма обмена ETH
-    usdt_amount_out = eth_amount_in * price_eth # сумма обмена USDT
+    # Рассчитываем комиссию в 5% от текущего баланса ETH
+    commission = balance_eth * 0.05
+    # Рассчитываем количество ETH для обмена, вычитая комиссию
+    eth_amount_in = balance_eth - commission
+    # Рассчитываем количество USDT, получаемое от обмена ETH
+    usdt_amount_out = eth_amount_in * price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_eth} ETH на {usdt_amount_out} USDT')
+    # Обновляем баланс USDT после обмена
     balance_usdt += usdt_amount_out
+    # Обновляем баланс ETH после обмена
     balance_eth -= eth_amount_in
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 else:
+    # Рассчитываем количество ETH, которое можно купить на текущий баланс USDT
     eth_amount_in = balance_usdt / price_eth
+    # Выводим информацию об обмене
     print(f'Обмен {balance_usdt} USDT на {eth_amount_in} ETH')
+    # Обновляем баланс ETH после обмена
     balance_eth += eth_amount_in
+    # Устанавливаем баланс USDT в 0 после обмена
     balance_usdt = 0
+    # Выводим обновленные балансы
     print(f'Баланс ETH: {balance_eth} ETH после обмена')
     print(f'Баланс USDT: {balance_usdt} USDT после обмена')
 
-print()
+print()  # Пустая строка для разделения выводов
