72821c784172bde2f5affb2a227bda137aa6d77c
Max Zarev <maksimelizarev@gmail.com>
2024-11-26 16:56:40 +0700

    обновление решения по метамаску

--- Быстрый результат/Metamask, сети, подключение, транзакция/решения/task3.py
+++ Быстрый результат/Metamask, сети, подключение, транзакция/решения/task3.py
@@ -7,6 +7,14 @@ def activity(bot: Bot):
     bot.metamask.auth_metamask()
     bot.metamask.select_chain(Chains.ARBITRUM_ONE)
     bot.ads.open_url('https://stargate.finance/bridge')
+    random_sleep(5, 7)
+
+    # перебираем страницы и закрываем всплывающее окно метамаска
+    for page in bot.ads.context.pages:
+        if 'notification' in page.url:
+            page.close()
+            break
+
     connect_wallet = bot.ads.page.get_by_role('button', name='Connect Wallet').filter(
         has=bot.ads.page.locator('svg'))
     if connect_wallet.count():
